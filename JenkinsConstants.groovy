9f7d778a7c20f83935bd7c30b88c0dd2d46acc01
valentin.kozlov <valentin.kozlov@kit.edu>
2024-03-13 13:25:26 +0100

    chore: update version to 0.6.1

--- JenkinsConstants.groovy
+++ JenkinsConstants.groovy
@@ -1,14 +1,14 @@
 import groovy.transform.Field
 // This file defines variables to be used in the AI4OS-Hub Upstream Jenkins pipeline
-// base_image   : base docker image for Dockerfile
-// base_gpu_tag : base docker image tag
+// base_cpu_tag : base docker image for Dockerfile, CPU version
+// base_gpu_tag : base docker image tag for Dockerfile, GPU version
 // dockerfile : what Dockerfile to use for building, can include path, e.g. docker/Dockerfile
 
-@Field
-def base_image = ''
+//@Field
+//def base_cpu_tag = '20.06-tf2-py3'
 
-@Field
-def base_tag = ''
+//@Field
+//def base_gpu_tag = '20.06-tf2-py3'
 
 @Field
 def dockerfile = 'Dockerfile'
